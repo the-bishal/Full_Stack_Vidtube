581e388498f36862a9faab56c2b741168f396615
Bishal Gupta <guptabishal111@gmail.com>
2025-01-01 14:39:11 +0530

    add login and logout

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import { User } from '../models/user.model.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating Access and Refresh Token")
+    }
+}
+
 const registerUser = asyncHandler( async (req, res) => {
     /* 
         Steps: 
@@ -88,6 +104,99 @@ const registerUser = asyncHandler( async (req, res) => {
         new ApiResponse(200, createdUser, "User registered Successfully")
     )
 
-} )
+})
 
-export { registerUser }
+const loginUser = asyncHandler( async (req, res) => {
+    /* 
+        Steps:
+          1. req.body -> data utao
+          2. use username/email
+          3. find the user on the basis of it
+          4. check password
+          5. if password matches then generate access and resfresh token
+          6. send cookies
+          7. send response
+    */
+
+    const {email, username, password} = req.body
+
+    if (!email || !username) {
+        throw new ApiError(400,"Username or Email is required")
+    }
+
+    if (!password) {
+        throw new ApiError(400,"Password is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User does not Exist")
+    }
+
+    // Note we can use User to access mongoose methods and user to access the methods made by our user
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid User Credentials")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser,
+                accessToken,
+                refreshToken,
+            },
+            "User logged In Successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler((req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User loggedout Successfully"))
+})
+
+export { 
+    registerUser,
+    loginUser,
+    logoutUser
+ }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { registerUser } from '../controllers/user.controller.js';
+import { registerUser, loginUser } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js'
 
 const router = Router();
@@ -17,5 +17,11 @@ router.route('/register').post(
     ]),
     registerUser
 )
+
+router.route('/login').post(loginUser)
+
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router
